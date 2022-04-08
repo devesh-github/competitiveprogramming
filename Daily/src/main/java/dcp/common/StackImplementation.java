14826d29748417d579125b73b1c9f337b8afa6a5
unknown <devdsolutionist@gmail.com>
2022-04-08 17:25:05 +0800

    Happy numbers

--- Daily/src/main/java/dcp/common/StackImplementation.java
+++ Daily/src/main/java/dcp/common/StackImplementation.java
@@ -2,31 +2,33 @@ package dcp.common;
 
 class CustomStack {
     public int top = 0, size;
-    public int[] stack;
+    public String[] stack;
 
     public void initialize(int size) {
-        stack = new int[size];
-        this.size = size;
+        stack = new String[size];
+        this.size = size-1;
     }
 
-    public void push(int ele) throws Exception {
-        if (top < size) {
+    public void push(String ele) throws Exception {
+        if (top <= size) {
             stack[top++] = ele;
         } else {
             throw new Exception("Stack is full");
         }
     }
 
-    public int pop() throws Exception {
+    public String pop() throws Exception {
         if (top != 0) {
-            return stack[--top];
+            String ele = stack[top];
+            top--;
+            return ele;
         } else {
             throw new Exception("Stack is empty");
         }
 
     }
 
-    public int peek() throws Exception {
+    public String peek() throws Exception {
         if (top != 0) {
             return stack[top];
         } else {
@@ -35,7 +37,7 @@ class CustomStack {
     }
 
     public int currentSize() throws Exception {
-        return top;
+        return top+1;
     }
 }
 
@@ -45,7 +47,7 @@ public class StackImplementation {
         CustomStack s = new CustomStack();
         s.initialize(10);
         try {
-            s.push(10);
+            /*s.push(10);
             s.push(20);
             s.push(30);
             s.push(40);
@@ -54,7 +56,13 @@ public class StackImplementation {
             s.push(40);
             s.push(40);
             s.push(60);
-            s.push(50);
+            s.push(50);*/
+
+            s.push("10");
+            s.push("20");
+            s.push("30");
+            s.push("40");
+            s.pop();
 
             System.out.println(s.pop());
             System.out.println(s.pop());
---
+++ Daily/src/main/java/ved/StringTest.java
@@ -0,0 +1,13 @@
+package ved;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+        String ab = "Hello team";
+        char[] ca = ab.toCharArray();
+        for(char c: ca) {
+            System.out.println(c);
+        }
+    }
+
+}
